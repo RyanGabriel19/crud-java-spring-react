e590ff3169f272131360745264abb970cfa275d4
Ryan <ryangabriel.apf19@gmail.com>
2025-11-13 15:17:16 -0300

    ajustando serviceusuario para hospedar front

--- frontend/src/service/serviceUsuario.js
+++ frontend/src/service/serviceUsuario.js
@@ -1,5 +1,5 @@
 // const API_URL = process.env.REACT_APP_API_URL;
-const API_URL ='http://localhost:8080/usuario';
+const API_URL = process.env.REACT_APP_API_URL + "/usuario";
 
 
 export const buscarUsuarioPorEmail = async (email) => {
